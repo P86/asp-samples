9c319105591d9e9eafb84627bcd88d1b0b3728ab
P86 <arkosp@gmail.com>
2021-02-18 19:00:25 +0100

    Add exaple of file streaming with range processing

---
+++ Streaming/Streaming/Controllers/MusicController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Streaming.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var fileName = "Assets/Movement.mp3";
+            if (!System.IO.File.Exists(fileName))
+            {
+                throw new InvalidOperationException($"File {fileName} does not exists");
+            }
+
+            return File(fileStream: System.IO.File.OpenRead(fileName),
+                contentType: "audio/mpeg",
+                enableRangeProcessing: true);
+        }
+    }
+}
