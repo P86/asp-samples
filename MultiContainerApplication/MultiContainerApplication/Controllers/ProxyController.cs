371e989cbe2e987e8bb9700d82a765651937bf1e
P86 <arkosp@gmail.com>
2021-04-27 18:14:42 +0200

    Add example of multi container app with docker conmpose

---
+++ MultiContainerApplication/MultiContainerApplication/Controllers/ProxyController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WebApi;
+
+namespace MultiContainerApplication.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProxyController : ControllerBase
+    {
+        private readonly IHttpClientFactory factory;
+
+        public ProxyController(IHttpClientFactory factory)
+        {
+            this.factory = factory;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            var client = factory.CreateClient("webapi");
+            var result = await client.GetAsync("/WeatherForecast");
+            
+            if(!result.IsSuccessStatusCode)
+            {
+                throw new Exception($"Status code does not indicate success. Status code: {result.StatusCode}");
+            }
+
+            return await JsonSerializer.DeserializeAsync<IEnumerable<WeatherForecast>>(await result.Content.ReadAsStreamAsync());
+        }
+    }
+}
---
+++ MultiContainerApplication/WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IConnectionMultiplexer connectionMultiplexer;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IConnectionMultiplexer connectionMultiplexer)
+        {
+            _logger = logger;
+            this.connectionMultiplexer = connectionMultiplexer;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            var database = connectionMultiplexer.GetDatabase();
+            var result = await database.StringGetAsync("forecasts");
+            if(result == RedisValue.Null)
+            {
+                var forecasts = GenerateForecasts();
+                await database.StringSetAsync("forecasts", JsonSerializer.Serialize(forecasts), TimeSpan.FromSeconds(10));
+                return forecasts;
+            }
+            
+            
+            return JsonSerializer.Deserialize<IEnumerable<WeatherForecast>>(result.ToString());
+        }
+
+        private static IEnumerable<WeatherForecast> GenerateForecasts()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
